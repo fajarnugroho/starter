edf8718141411c27a3165fe5b0b7b750ca74e545
Fajar Nugroho <fajar@nugrohof.id>
2024-10-15 21:35:55 +0800

    update: enable js and ts syntax highlighting

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,3 @@
+require("tree-sitter-typescript").typescript() -- TypeScript grammar
+require("tree-sitter-typescript").tsx() -- TSX grammar
+require("tree-sitter-javascript").javascript()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,17 +9,36 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+	  require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+   "williamboman/mason.nvim",
+   opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
+	  "pyre"
+    },
+   },
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim",
+			"lua",
+			"vimdoc",
+			"html",
+			"css",
+			"python"
+  		},
+  	},
+  },
 }
