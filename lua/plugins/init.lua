c712e945848b35e041407c24464df37e938f91a0
Fajar Nugroho <fajar@nugrohof.id>
2024-10-19 20:40:22 +0800

    fix: indentation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,53 +1,53 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require "configs.conform",
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-	  require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require "configs.lspconfig"
+		end,
+	},
 
-  {
-   "williamboman/mason.nvim",
-   opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "stylua",
-      "html-lsp",
-      "css-lsp",
-      "prettier",
-	  "pyre",
-	  "terraformls",
-	  "tflint",
-	  "bash-language-server",
-	  "json-lsp",
-	  "yaml-language-server"
-    },
-   },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"stylua",
+				"html-lsp",
+				"css-lsp",
+				"prettier",
+				"pyre",
+				"terraformls",
+				"tflint",
+				"bash-language-server",
+				"json-lsp",
+				"yaml-language-server"
+			},
+		},
+	},
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim",
-			"lua",
-			"vimdoc",
-			"html",
-			"css",
-			"python",
-			"terraform",
-			"bash",
-			"json",
-			"yaml"
-  		},
-  	},
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"python",
+				"terraform",
+				"bash",
+				"json",
+				"yaml"
+			},
+		},
+	},
 }
